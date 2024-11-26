d42c18728b32cc7d47f9a72e6b6c37b9cd0e6076
quang.ly <141037793+G1n305@users.noreply.github.com>
2024-11-26 09:45:44 +0700

    Update example.js

--- example.js
+++ example.js
@@ -19,20 +19,18 @@ console.log("starting...");
 while (true) {
     led_blink();
     _delay(1000);
-    printLastOnetag();
-    //reset_tag_list();
-    //printCurrentOnetag();
-    //CheckCRC(ctx, RecvBuff, RecvBuff[0]);
-    _delay(1000);
-    printCurrentOnetag();
-    _delay(1000);
-    getTakeIn();
-    getTakeOut();
-    _delay(1000);
-    // printInformation();
+    console.log("starting...");
+    // printLastOnetag();
     // _delay(1000);
-    detroyInventory();
-    reset_tag_list();
-    _delay(1000);
+    // printCurrentOnetag();
+    // _delay(1000);
+    // getTakeIn();
+    // getTakeOut();
+    // _delay(1000);
+    // // printInformation();
+    // // _delay(1000);
+    // detroyInventory();
+    // reset_tag_list();
+    //_delay(1000);
 }
 
