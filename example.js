86a5d2d51ed637b325bd1aabdc9e024780f2762c
quang.ly <141037793+G1n305@users.noreply.github.com>
2024-11-28 16:36:59 +0700

    Update example.js

--- example.js
+++ example.js
@@ -15,15 +15,15 @@
 // askUserAndExecute(1)
 console.log("starting...");
 while (true) {
-    led_blink();
-    _delay(1000);
-    getNumTagCurrent();
-    _delay(1000);
-    getNumTagLast();
-    _delay(1000);
-    printLastOnetag();
-    _delay(1000);
-    printCurrentOnetag();
-    _delay(1000);
+    // led_blink();
+     _delay(1000);
+    // getNumTagCurrent();
+    // _delay(1000);
+    // getNumTagLast();
+    // _delay(1000);
+    // printLastOnetag();
+    // _delay(1000);
+    // printCurrentOnetag();
+    // _delay(1000);
 }
 
